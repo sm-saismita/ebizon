a80c5a0c017a5633a4f5a0b9fb4081e7ce347ea8
sais1994 <54997593+sm-saismita@users.noreply.github.com>
2021-09-29 20:29:49 +0530

    Delete src directory

--- src/components/userDetails/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Account index - export default Account component here
- */
-
-export {default} from './UserDetailsView.js';
--- src/library/AppConfig.js
+++
@@ -1,8 +0,0 @@
-/**
- * AppConfig.js
- * basic server data like base path, url will initialized
- */
-
- export const appConfig = {
-  base_path: "https://randomuser.me/"
- };
